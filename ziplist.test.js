fb0f3a86a228435a0ecf6ef5092a769f331247f7
kodayv <kodayv@hawaii.edu>
2017-04-14 15:08:07 -1000

    update
    added deep list testing

--- ziplist.test.js
+++ ziplist.test.js
@@ -8,9 +8,15 @@ describe('zipList', function () {
   it('should return a single array of six elements', function () {
     chai.expect(zipList(One, Two)).to.have.length(6);
   });
+  it('should deep equal the passed six element array', function () {
+    chai.expect(zipList(One, Two)).to.deep.equal(['a', 1, 'b', 2, 'c', 3]);
+  });
 });
 describe('zipListTheSimpleWay', function () {
   it('should return a single array of six elements', function () {
     chai.expect(zipListTheSimpleWay(One, Two)).to.have.length(6);
+  });
+  it('should deep equal the passed six element array', function () {
+    chai.expect(zipListTheSimpleWay(One, Two)).to.deep.equal(['a', 1, 'b', 2, 'c', 3]);
   });
 });
